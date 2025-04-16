138499b17128dfb0a904dd92e4df01eab733fa3f
신우찬 <8621skyblue@naver.com>
2025-04-17 01:25:42 +0900

    로그인 화면 기초

---
+++ src/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./login.css";
+
+function Login() {
+  return (
+    <div classname="login_basic">
+      <h1 stylename="login_title">광운대학교 행복기숙사</h1>
+      <div>로그인</div>
+    </div>
+  );
+}
+
+export default Login;
