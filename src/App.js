02d9273e3a233d23e0da990172f866d56d87679f
신우찬 <8621skyblue@naver.com>
2025-04-17 01:25:56 +0900

    메인 화면 기초초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="main_title">기숙사 시설 예약 시스템</h1>
+      <div className="title_line" />
     </div>
   );
 }
