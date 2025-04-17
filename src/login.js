aca2ab13ddf2f90ba805a6c0f13a68a4a342d7cc
신우찬 <8621skyblue@naver.com>
2025-04-17 17:18:15 +0900

    class 수정 및 입력칸 추가가

--- src/login.js
+++ src/login.js
@@ -3,9 +3,12 @@ import "./login.css";
 
 function Login() {
   return (
-    <div classname="login_basic">
-      <h1 stylename="login_title">광운대학교 행복기숙사</h1>
-      <div>로그인</div>
+    <div class="login_basic">
+      <h1 class="login_title">광운대학교 행복기숙사</h1>
+      <div>학번</div>
+      <input></input>
+      <div>비밀번호</div>
+      <input type="password"></input>
     </div>
   );
 }
