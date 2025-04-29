777e9c307e5f7726fd8dccc486b7e32e6f46ae25
신우찬 <8621skyblue@naver.com>
2025-04-29 19:54:25 +0900

    로그인 화면 구상상

--- src/login.js
+++ src/login.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./login.css";
 
 function Login() {
   return (
-    <div class="login_basic">
-      <h1 class="login_title">광운대학교 행복기숙사</h1>
-      <div>학번</div>
-      <input></input>
-      <div>비밀번호</div>
-      <input type="password"></input>
-        <button class="login_button">로그인</button>
+    <div className="height">
+      <div className="login_basic">
+        <h1 className="login_title">광운대학교 행복기숙사</h1>
+        <div>학번</div>
+        <input></input>
+        <div>비밀번호</div>
+        <input type="password"></input>
+        <br />
+        <button>로그인</button>
+      </div>
     </div>
   );
 }
