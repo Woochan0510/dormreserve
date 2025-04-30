5ce766438e30be59fe8b8f9d1649d195cabaecf8
신우찬 <8621skyblue@naver.com>
2025-04-30 11:14:42 +0900

    파일 확장자 변경경

---
+++ src/login.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="height">
+      <div className="login_basic">
+        <h1 className="login_title">광운대학교 행복기숙사</h1>
+        <div>학번</div>
+        <input></input>
+        <div>비밀번호</div>
+        <input type="password"></input>
+        <br />
+        <button
+          onClick={() => {
+            navigate("/Main");
+          }}
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.css";
+
+function Main() {
+  return (
+    <div>
+      <h1 className="main_title">기숙사 시설 예약 시스템</h1>
+      <div className="title_line" />
+    </div>
+  );
+}
+
+export default Main;
