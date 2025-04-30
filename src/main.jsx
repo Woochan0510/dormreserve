d737914bbd4f8993b9977a1cc9310ca762a8c93a
신우찬 <8621skyblue@naver.com>
2025-04-30 14:02:18 +0900

    로그아웃 버튼 / 취사실 이동 기능

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./main.css";
 
 function Main() {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1 className="main_title">기숙사 시설 예약 시스템</h1>
+      <div className="top_bar">
+        <h1 className="main_title">기숙사 시설 예약 시스템</h1>
+        <button onClick={() => navigate("/")}>로그아웃</button>
+      </div>
       <div className="title_line" />
+      <div>
+        <div onClick={() => navigate("/book/cook")}>취사실 예약</div>
+      </div>
     </div>
   );
 }
