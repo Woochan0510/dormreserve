3bc9d4ee71b40c2b692126cd79bfde353c1d39a9
신우찬 <8621skyblue@naver.com>
2025-04-30 14:01:44 +0900

    취사실 예약 사이트 url 추가

--- src/shared/router.js
+++ src/shared/router.js
@@ -2,12 +2,14 @@ import react from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../login";
 import Main from "../main";
+import Cook from "../cook";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="Main" element={<Main />} />
+      <Route path="main" element={<Main />} />
+      <Route path="book/cook" element={<Cook />} />
     </Routes>
   );
 };
