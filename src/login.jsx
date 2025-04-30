2ff3bf777f2585a3875e0f67add87039ea2b9f25
신우찬 <8621skyblue@naver.com>
2025-04-30 12:48:53 +0900

    API 이용한 로그인 로직 구현현

--- src/login.jsx
+++ src/login.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
 
 function Login() {
   const navigate = useNavigate();
+  const [studentId, setStudentId] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "https://f7j9sz5d-8000.asse.devtunnels.ms/api/v1/users/login/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            student_id_number: studentId,
+            password: password,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        localStorage.setItem("token", data.token);
+        navigate("/Main");
+      } else if (!studentId) {
+        setError("아이디를 입력해주세요.");
+      } else if (!password) {
+        setError("비밀번호를 입력해주세요.");
+      } else {
+        setError("아이디 또는 비밀번호가 틀렸습니다.");
+      }
+    } catch (err) {
+      setError("오류 발생");
+    }
+  };
 
   return (
     <div className="height">
       <div className="login_basic">
         <h1 className="login_title">광운대학교 행복기숙사</h1>
-        <div>학번</div>
-        <input></input>
-        <div>비밀번호</div>
-        <input type="password"></input>
+        <label>학번</label>
+        <input
+          value={studentId}
+          onChange={(e) => setStudentId(e.target.value)}
+        />
+        <label>비밀번호</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br />
-        <button
-          onClick={() => {
-            navigate("/Main");
-          }}
-        >
-          로그인
-        </button>
+        <button onClick={handleLogin}>로그인</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
     </div>
   );
