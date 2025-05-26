1cd4e085aab690465348975344429cfe2f0c037e
신우찬 <8621skyblue@naver.com>
2025-05-26 19:32:10 +0900

    로그아웃 로직 구현중

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,34 @@ import url from "./url.jsx";
 
 function Main() {
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(url + "api/v1/users/logout/", {
+        method: "POST",
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        const data = await response.json();
+        console.log("error:", data);
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
   return (
     <div>
       <div className="top_bar">
         <h1 className="main_title">기숙사 시설 예약 시스템</h1>
-        <button onClick={() => navigate("/")}>로그아웃</button>
+        <button onClick={handleLogout}>로그아웃</button>
       </div>
       <div className="title_line" />
       <div>
