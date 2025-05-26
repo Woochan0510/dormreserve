b009e5e4b8c8d9a23f42c900bec39f97efe65ae6
신우찬 <8621skyblue@naver.com>
2025-05-27 07:33:45 +0900

    라운지 예약 화면 구현중

---
+++ src/lounge.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import "./CookGrid.css";
+import url from "./util.jsx";
+import axios from "axios";
+import "./lounge.css";
+import { useNavigate } from "react-router-dom";
+
+function Lounge() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="base">
+      <div className="top_bar">
+        <div className="top_bar_name">헬스장 예약</div>
+        <div className="top_right">
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "green" }} />
+            <div>사용가능</div>
+          </div>
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "red" }} />
+            <div>사용중</div>
+          </div>
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "yellow" }} />
+            <div>수리중</div>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="map">여기에 삽입</div>
+        <button onClick={() => navigate("/main/")} className="main_button">
+          메인화면
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Lounge;
