247fb0e9c3d54154acfbb814cf650529ff8ae4c6
신우찬 <8621skyblue@naver.com>
2025-05-27 07:29:51 +0900

    루트 추가

--- src/shared/router.js
+++ src/shared/router.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../login";
 import Main from "../main";
 import Cook from "../cook";
+import Lounge from "../lounge";
+import Gym from "../gym";
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Route path="/" element={<Login />} />
       <Route path="main" element={<Main />} />
       <Route path="book/cook" element={<Cook />} />
+      <Route path="book/gym" element={<Gym />} />
+      <Route path="book/lounge" element={<Lounge />} />
     </Routes>
   );
 };
