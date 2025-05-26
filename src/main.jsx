89e02e7c461e47a75167e52dc6cb71b84d7e1528
신우찬 <8621skyblue@naver.com>
2025-05-27 06:32:54 +0900

    로그아웃 로직 구현

--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,48 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./main.css";
 import url from "./util.jsx";
+import axios from "axios";
 
 function Main() {
   const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      const response = await fetch(url + "api/v1/users/logout/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      });
-
-      if (response.status === 200) {
-        navigate("/");
-      } else {
-        const data = await response.json();
-        console.log("error:", data);
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // 쿠키 이름이 일치하면 값을 디코딩하여 반환
+        if (cookie.startsWith(name + "=")) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
       }
-    } catch (err) {
-      console.log("error", err);
     }
+    return cookieValue;
+  }
+
+  const handleLogout = async () => {
+    axios
+      .post(
+        `${url}api/v1/users/logout/`,
+        {},
+        {
+          headers: {
+            "X-CSRFToken": getCookie("csrftoken") || "",
+            "Content-Type": "application/json", // CSRF 토큰 전송
+          },
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        console.log("로그아웃 성공:", response.data);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("로그아웃 실패:", error.response.data);
+        // 에러 처리
+      });
   };
 
   return (
