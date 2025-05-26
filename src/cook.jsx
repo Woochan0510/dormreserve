9c3dc32b69802e3bc9a30dce6b5527c995ce5737
신우찬 <8621skyblue@naver.com>
2025-05-26 19:31:52 +0900

    취사실 예약 구현중

--- src/cook.jsx
+++ src/cook.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./cook.css";
 
 function Cook() {
-  return <div>취사실 예약</div>;
+  return (
+    <div clasName="base">
+      <div className="top_bar">
+        <div className="top_title_name">취사실 예약</div>
+      </div>
+      <div className="main">
+        <div className="map"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Cook;
