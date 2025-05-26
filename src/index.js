73fb473afb44026bb0263b31c789b01c9886a4f7
신우찬 <8621skyblue@naver.com>
2025-05-27 02:08:28 +0900

    엄격모드 해제

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import Login from "./login";
-import App from "./main";
 import Router from "./shared/router";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
