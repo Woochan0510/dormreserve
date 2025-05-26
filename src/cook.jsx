9f68bcd155afb012f7aa66a48de1366406e6259b
신우찬 <8621skyblue@naver.com>
2025-05-27 05:09:28 +0900

    취사실 구현 완료

--- src/cook.jsx
+++ src/cook.jsx
@@ -7,6 +7,20 @@ function Cook() {
     <div className="base">
       <div className="top_bar">
         <div className="top_title_name">취사실 예약</div>
+        <div className="top_right">
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "green" }} />
+            <div>사용가능</div>
+          </div>
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "red" }} />
+            <div>사용중</div>
+          </div>
+          <div className="top_right_set">
+            <div className="circle" style={{ backgroundColor: "yellow" }} />
+            <div>수리중</div>
+          </div>
+        </div>
       </div>
       <div className="main">
         <div className="map">
